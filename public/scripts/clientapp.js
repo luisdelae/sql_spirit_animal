bbf45eeafd61ce1fff25d9a530a89802271a2dfe
user <user@users-MacBook-Pro.local>
2016-02-12 15:59:58 -0600

    added assigment button and added stylesheet

--- public/scripts/clientapp.js
+++ public/scripts/clientapp.js
@@ -1,10 +1,10 @@
-//var nameArray = [];
-//var animalArray = [];
+var randomName;
+var randomAnimal;
 
 $(document).ready(function() {
-    $('#post-data-name').on('load', clickGetNameData());
+    $('#post-data-name').on('load', loadGetNameData());
 
-    $('#post-data-name').on('load', clickGetAnimalData());
+    $('#post-data-name').on('load', loadGetAnimalData());
 
     $('#post-data-name').on('click', clickPostNameData);
 
@@ -13,6 +13,8 @@ $(document).ready(function() {
     $('#get-data-name').on('click', clickGetNameData);
 
     $('#get-data-animal').on('click', clickGetAnimalData);
+
+    $('#spirit-animal-assigner').on('click', assignSpiritAnimal);
 
 });
 
@@ -35,12 +37,15 @@ var clickPostNameData = function() {
         },
         success: function (data) {
             console.log('From Server: ', data);
+
+            randomName = data;
+
             $('.name-test').text(data.join(', '));
         }
     })
 };
 
-function clickGetNameData() {
+function loadGetNameData() {
     event.preventDefault();
     $.ajax({
         type: 'GET',
@@ -50,6 +55,24 @@ function clickGetNameData() {
             //nameArray = data;
             var appendNewDiv = '<div class="name-test">' + data.join(', ') + '</div>';
             $('#names-and-animals').append(appendNewDiv);
+        }
+    });
+}
+
+function clickGetNameData() {
+
+    event.preventDefault();
+    $.ajax({
+        type: 'GET',
+        url: '/name',
+        success: function(data) {
+            console.log(data);
+            var nameArray = data;
+
+            var nameIndex = randomIndex(0, nameArray.length - 1);
+
+            $('#names-and-animals').append('<div>Random Name: ' + nameArray[nameIndex] + '</div>');
+
         }
     });
 }
@@ -73,12 +96,13 @@ var clickPostAnimalData = function() {
         },
         success: function (data) {
             console.log('From Server: ', data);
+            randomAnimal = data;
             $('.animal-test').text(data.join(', '));
         }
     })
 };
 
-function clickGetAnimalData() {
+function loadGetAnimalData() {
     event.preventDefault();
     $.ajax({
         type: 'GET',
@@ -89,4 +113,37 @@ function clickGetAnimalData() {
             $('#names-and-animals').append(appendNewDiv);
         }
     });
+}
+
+function clickGetAnimalData() {
+    event.preventDefault();
+    $.ajax({
+        type: 'GET',
+        url: '/animal',
+        success: function(data) {
+            console.log(data);
+            //var appendNewDiv = '<div class="animal-test">' + data.join(', ') + '</div>';
+            //$('#names-and-animals').append(appendNewDiv);
+
+            var animalArray = data;
+
+            var animalIndex = randomIndex(0, animalArray.length - 1);
+
+            $('#names-and-animals').append('<div>Random Animal: ' + animalArray[animalIndex] + '</div>');
+        }
+    });
+}
+
+function randomIndex(min, max) {
+    return Math.floor(Math.random() * (1 + max - min) + min);
+}
+
+function assignSpiritAnimal() {
+
+    var nameIndex = randomIndex(0, randomName.length - 1);
+    var animalIndex = randomIndex(0, randomAnimal.length - 1);
+
+    console.log(randomName[nameIndex]);
+
+    $('#names-and-animals').append('<div class="animal-assignment">Spirit Animal Assignment: ' + randomName[nameIndex] + " the " + randomAnimal[animalIndex] + '</div>')
 }
